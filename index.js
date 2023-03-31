a720fcbbfe29568b4d2c61b3d27cdb953a4617fa
adeyemi ademola <ademolaadeyemieric@gmail.com>
2023-03-31 18:49:31 +0100

    fully functional

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const openBtn = document.getElementById('open-btn');
+const response = document.getElementById('response');
 const modelContainer = document.getElementById('model-container');
 const closeBtn = document.getElementById('close-btn');
+const request = document.querySelector('.request');
+const palindome = document.getElementById('palindome');
+const answer = document.getElementById('answer');
 
-openBtn.addEventListener('click', function(){
-    modelContainer.style.display = 'block';
+
+response.addEventListener('click', function(){
+    const checkPalindome = palindome.value.split("").reverse().join("");
+
+    if (checkPalindome === "") {
+        request.style.color = 'blue';
+        modelContainer.style.display = 'block';
+        answer.innerText = 'Please Input a Value';
+    } else if (checkPalindome === palindome.value) {
+        request.style.color = 'green';
+        modelContainer.style.display = 'block';
+        answer.innerText = 'Correct, The Input is a Palindome';
+    } else {
+        request.style.color = 'red';
+        modelContainer.style.display = 'block';
+        answer.innerText = 'Try Again, The Input is  Not a Palindome';
+    }
 });
 
 closeBtn.addEventListener('click', function(){
@@ -15,24 +33,3 @@ window.addEventListener('click', function(e){ //you can also asign to document
        modelContainer.style.display = 'none';
    }
 });
-
-function checkWord() {
-    const palindome = document.getElementById('palindome').value
-    const invalid = document.getElementById('invalid')
-    const wrong = document.getElementById('wrong')
-    const right = document.getElementById('right')
-
-    const forwardWord = palindome.split("");
-    const reverseWord = forwardWord.reverse();
-    const joinReverseWord = reverseWord.join("");
-
-
-    if (palindome === "") {
-        invalid.hidden = false;
-    } else if (palindome === joinReverseWord) {
-        right.hidden = false;
-    } else {
-        wrong.hidden = false;
-    }
-
-}
